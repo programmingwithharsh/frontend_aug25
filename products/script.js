0451b0f957239fb8414548f301a9d4ef4661b5eb
Harsh Tiwari <harshtiwari343@email.com>
2025-08-13 11:38:31 +0530

    Added Get Individual, Update and Delete API calls

--- products/script.js
+++ products/script.js
@@ -30,7 +30,14 @@ const loadProducts = () => {
     and refresh page
 */
 const deleteProduct = () => {
-    alert(deleteProductId);
+    fetch(`${apiUrl}/${deleteProductId}`, {
+        method: 'DELETE',
+    })
+        .then((response) => response.json())
+        .then(() => {
+            alert(`Product Deleted Successfully`);
+            window.location.href = "./ProductList.html"; // redirect to products page
+        });
 }
 
 const storeDeleteId = (id) => {
@@ -40,11 +47,52 @@ const storeDeleteId = (id) => {
 // GET Individual product based on id
 const filterProductById = (id) => {
     // Filter products based on url id and display result in HTML Form value
+    return fetch(`${apiUrl}/${id}`)
+        .then((response) => response.json())
+        .then((product) => {
+            // Populate form data with product data
+            document.getElementById("productName").value = product.productName;
+            document.getElementById("productCode").value = product.productCode;
+            document.getElementById("releaseDate").value = product.releaseDate;
+            document.getElementById("description").value = product.description;
+            document.getElementById("price").value = product.price;
+            document.getElementById("starRating").value = product.starRating;
+            document.getElementById("imageUrl").value = product.imageUrl;
+        });
 }
 
 // PUT API Call, http://localhost:4000/api/products
-const updateProduct = (id) => {
-    return true
+const updateProduct = () => {
+    const id = getIdFromUrl();
+    let productName = document.getElementById("productName").value;
+    let productCode = document.getElementById("productCode").value;
+    let releaseDate = document.getElementById("releaseDate").value;
+    let description = document.getElementById("description").value;
+    let price = document.getElementById("price").value;
+    let starRating = document.getElementById("starRating").value;
+    let imageUrl = document.getElementById("imageUrl").value;
+
+    // Javascript Form validation
+    if (productName === "") {
+        alert("Please enter product name");
+        return false;
+    }
+
+    let product = { productName, productCode, releaseDate, description, price, starRating, imageUrl };
+    console.log(product);
+    fetch(`${apiUrl}/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(product),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then(() => {
+            alert(`Product Updated Successfully`);
+            window.location.href = "./ProductList.html"; // redirect to products page
+        });
+    return false;
 }
 
 const addProduct = () => {
@@ -54,7 +102,7 @@ const addProduct = () => {
     let releaseDate = document.getElementById("releaseDate").value;
     let description = document.getElementById("description").value;
     let price = document.getElementById("price").value;
-    let startRating = document.getElementById("startRating").value;
+    let starRating = document.getElementById("starRating").value;
     let imageUrl = document.getElementById("imageUrl").value;
 
     // Javascript Form validation
@@ -69,7 +117,7 @@ const addProduct = () => {
         "releaseDate": releaseDate,
         "description": description,
         "price": price,
-        "startRating": startRating,
+        "starRating": starRating,
         "imageUrl": imageUrl
     }
 
@@ -109,7 +157,7 @@ const displayProducts = () => {
         <td>${product.releaseDate}</td>
         <td>${product.description}</td>
         <td>${product.price}</td>
-        <td>${product.startRating}</td>
+        <td>${product.starRating}</td>
         <td><a href="./EditProduct.html?${product._id}">Edit</a>|
             <a data-bs-toggle="modal" onClick="storeDeleteId('${product._id}')" data-bs-target="#deleteModal" href="">Delete</a>
         </td>`
